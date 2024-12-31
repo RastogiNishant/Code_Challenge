61f07929566a62a59b2f6aa629703fd0e3f86ba4
NV Rastogi <rastoginishant@yahoo.in>
2024-12-31 11:27:13 +0530

    Problem 1

---
+++ Problem 1/index.js
@@ -0,0 +1,27 @@
+// Provide 3 unique implementations of the following function in JavaScript.
+
+// **Input**: `n` - any integer
+
+// *Assuming this input will always produce a result lesser than `Number.MAX_SAFE_INTEGER`*.
+
+// ** Output **: `return` - summation to`n`, i.e. `sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15`.
+
+var sum_to_n_a = function (n) {
+	return Array.from({ length: n }, (_, i) => i + 1).reduce(
+		(acc, curr) => acc + curr,
+		0,
+	);
+};
+
+var sum_to_n_b = function (n) {
+	return (n * (n + 1)) / 2;
+};
+
+var sum_to_n_c = function (n) {
+	if (n <= 0) return 0;
+	return n + sum_to_n_c(n - 1);
+};
+
+console.log(sum_to_n_a(-1));
+console.log(sum_to_n_b(5));
+console.log(sum_to_n_c(15));
